a7956dc589e2883bde1a549ceff8f85e13a06897
Ana Beatriz <biamaiaribeiro@gmail.com>
2023-04-02 11:32:19 -0300

    login

---
+++ Login/senha.js
@@ -0,0 +1,26 @@
+// Pegando os valores da página HTML
+const passwordInput = document.getElementById("password");
+const eyeSvg = document.getElementById("eyeSvg");
+
+// Atribuindo a ação
+function eyeClick() {
+  let inputTypeIsPassword = passwordInput.type == "password";
+
+  if (inputTypeIsPassword) {
+    showPassword();
+  } else {
+    hidePassword();
+  }
+}
+
+// Função mostrar senha
+function showPassword() {
+  passwordInput.setAttribute("type", "text");
+  eyeSvg.setAttribute("src", "closed.svg");
+}
+
+//Função esconder senha
+function hidePassword() {
+  passwordInput.setAttribute("type", "password");
+  eyeSvg.setAttribute("src", "open.svg");
+}
